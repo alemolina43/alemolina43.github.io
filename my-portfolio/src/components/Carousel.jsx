1d88364b1963b05845ebf3f0d5cd3fafa8315319
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-06-04 18:49:55 -0700

    Add carousel component

---
+++ my-portfolio/src/components/Carousel.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Carousel = () => {
+  return (
+    <section>
+      <div>content</div>
+      <div>carousel</div>
+    </section>
+  );
+};
+
+export default Carousel;
