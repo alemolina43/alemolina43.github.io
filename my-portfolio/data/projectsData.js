458cd0233d8f9dfbb40a2b7bcb0e1283313dc4b4
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-06-05 16:20:28 -0700

    Implement slider for projects:

--- my-portfolio/data/projectsData.js
+++ my-portfolio/data/projectsData.js
@@ -2,37 +2,45 @@ import MunchMatch from "../src/assets/munchMatch.png";
 import Quizzas from "../src/assets/quizzas.png";
 import Tweeter from "../src/assets/tweeter.png";
 import TinnyApp from "../src/assets/tinyApp.png";
+import FrontendMentor from "../src/assets/frontend_mentor.png";
 
 const projectsData = [
   {
-    name: "MunchMatch",
-    link: "https://github.com/J-0-d-a-1/munchMatch",
+    title: "MunchMatch",
+    url: "https://github.com/J-0-d-a-1/munchMatch",
     thumbNail: MunchMatch,
     description:
       "Swipe-style app for discovering and favoriting dishes; features include menu browsing, owner dashboards, and image uploads.",
   },
 
   {
-    name: "Quizzas",
-    link: "https://github.com/vikkisupurrbe/midtermProject",
+    title: "Quizzas",
+    url: "https://github.com/vikkisupurrbe/midtermProject",
     thumbNail: Quizzas,
     description:
       "Collaborative quiz platform to create, attempt, and share quizzes.",
   },
 
   {
-    name: "Tweeter",
-    link: "https://github.com/alemolina43/tweeter",
+    title: "Tweeter",
+    url: "https://github.com/alemolina43/tweeter",
     thumbNail: Tweeter,
     description:
       "Responsive single-page Twitter clone with live character counter and error handling.",
   },
 
   {
-    name: "TinyApp",
-    link: "https://github.com/alemolina43/tinyapp",
+    title: "TinyApp",
+    url: "https://github.com/alemolina43/tinyapp",
     thumbNail: TinnyApp,
     description: "URL shortener with full user authentication.",
+  },
+
+  {
+    title: "Frontend Mentor",
+    url: "https://www.frontendmentor.io/profile/alemolina43",
+    thumbNail: FrontendMentor,
+    description: "Platform with practical, frontend project-based challenges.",
   },
 ];
 
--- my-portfolio/src/components/Carousel.jsx
+++ my-portfolio/src/components/Carousel.jsx
@@ -1,10 +1,78 @@
 import React from "react";
+import "../../styles/Carousel.scss";
+import projectsData from "../../data/projectsData";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  Autoplay,
+  Pagination,
+  Navigation,
+  EffectCoverflow,
+} from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 const Carousel = () => {
   return (
-    <section>
-      <div>content</div>
-      <div>carousel</div>
+    <section className="carousel" id="projects">
+      <div>
+        <div className="carousel-content">
+          <h1>Take a look at my projects</h1>
+        </div>
+      </div>
+      <Swiper
+        navigation={true}
+        modules={[Navigation, Pagination, EffectCoverflow, Autoplay]}
+        className="myswiper"
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 3,
+          slideShadows: true,
+        }}
+        loop={true}
+        pagination={{ clickable: true }}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 1,
+          },
+          1024: {
+            slidesPerView: 2,
+          },
+          1560: {
+            slidesPerView: 2,
+          },
+        }}
+      >
+        {projectsData.map((project) => (
+          <SwiperSlide
+            key={project.title}
+            className="myswiper-slider"
+            style={{ backgroundImage: `url(${project.thumbNail}) ` }}
+          >
+            <div className="slide-content">
+              <h2>{project.title}</h2>
+              <p>{project.description}</p>
+              <a
+                href={`${project.url}`}
+                target="_blank"
+                className="slider-btn"
+                rel="noreferrer"
+              >
+                explore
+              </a>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </section>
   );
 };
